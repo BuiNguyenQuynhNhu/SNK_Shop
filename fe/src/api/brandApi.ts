efa952d3ac662770f313ca369009f0598206fa19
Bùi Nguyễn Quỳnh Như <153304029+BuiNguyenQuynhNhu@users.noreply.github.com>
2026-05-01 00:04:07 +0700

    init fe

--- fe/src/api/brandApi.ts
+++
@@ -1,6 +0,0 @@
-const API = "http://localhost:3000/api";
-
-export const getBrands = async () => {
-  const res = await fetch(`${API}/brands`);
-  return res.json();
-};
--- fe/src/api/productApi.ts
+++
@@ -1,5 +0,0 @@
-import { api } from "./api";
-
-export const getProducts = () => api("/products");
-
-export const getProductById = (id: number) => api(`/products/${id}`);
