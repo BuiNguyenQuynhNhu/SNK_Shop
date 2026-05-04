e79e26a0e911235337ef9925be8fe92e8ddf4c94
Bùi Nguyễn Quỳnh Như <153304029+BuiNguyenQuynhNhu@users.noreply.github.com>
2026-05-04 23:51:38 +0700

    fix load azure image error

--- fe/components/layout/footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-white border-t py-4 text-center text-sm text-gray-500">
-      © 2026 SNK_Shop, Inc. All rights reserved
-    </footer>
-  );
-};
-
-export default Footer;
